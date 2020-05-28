09c174accc5084fbbd6be9d8faa5167aeb6d5944
ZachHoskins <zach9414@gmail.com>
2020-05-27 22:40:27 -0500

    Day 2

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,16 @@
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+export const decodedValue = ([color1, color2]) => {
+  return COLORS.indexOf(color1)*10 + COLORS.indexOf(color2);
+};
